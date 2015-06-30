0b7b635c9ee282082d9f116e25bedaa0e9bacf52
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-06-30 20:49:40 +0200

    update clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/rasmuserik/util"
 
   :dependencies 
-  [[org.clojure/clojure "1.7.0-RC1"]
+  [[org.clojure/clojure "1.7.0"]
    [org.clojure/clojurescript "0.0-3308"]
    [cljsjs/react "0.13.3-0"]
    [com.cognitect/transit-cljs "0.8.220"]
