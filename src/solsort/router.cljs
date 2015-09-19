3fbbe16b11539c28ee559cc2b9d1913089d53ea8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-09-19 08:16:08 +0200

    cleanup

--- src/solsort/router.cljs
+++ src/solsort/router.cljs
@@ -41,8 +41,6 @@
     [cljs.test :refer-macros  [deftest testing is run-tests]]
     [re-frame.core :as re-frame :refer [register-sub subscribe register-handler dispatch dispatch-sync]]
     [solsort.misc :as misc :refer [function? chan? log]]
-    [solsort.ui :as ui]
-    [solsort.style :as style]
     [reagent.core :as reagent :refer  []]))
 
 ;; # Util
@@ -66,6 +64,17 @@
                   args)]
     (into {"route" path} args)))
 
+;; # Route management
+(defonce routes (atom {}))
+(defn route [id f] (swap! routes assoc id f))
+(defn get-route [id] 
+  (fn [_]
+    (go
+      {:type :html
+       :content "hello world"
+       :title "hi"
+       :html [:h1 "hello"]})))
+
 ;; # Dispatch-types
 ;;
 ;; There are the following possible dispatch-types:
@@ -82,7 +91,6 @@
 ;; NB: `(solsort.router/start)` should be called on page load, and on any page-change
 ;; that might add widgets.
 ;;
-(declare get-route)
 (defn <extract-route [data]
   (go (let [content (get-route (get data "route" ""))
             content (if (function? content) (content data) content)
@@ -122,81 +130,12 @@
   (go (let [data (<! (<extract-route data))]
         (if (not= :html (:type data)) (:content data) (html->content data)))))
 (go (log (<! (<dispatch-route {"route" "hello"}))))
-;; # Route management
-(defn get-route [] 
-  (fn [_]
-    (go
-      {:type :html
-       :content "hello world"
-       :title "hi"
-       :html [:h1 "hello"]})))
-
-;; # Handlers and subscriptions
-
-(register-handler :route (fn [db [_ route] _] (into db route)))
-(register-sub :app (fn [db _] (reaction (first (:path @db)))))
-
-;; # router
-(defonce routes (atom {}))
-(defn route [id & {:keys (html app json http f)
-                   :or {f (or html app json http)}}]
-  (swap! routes assoc id f))
-
-(defn parse-route [adr]
-  (let [path (nth (re-matches route-re adr) 1)
-        args (.split (.slice adr (inc (.-length path))) "&")
-        path (.split path #"[./]")
-        args (map #(let [i (.indexOf % "=")]
-                     (if (= -1 i)
-                       [% true]
-                       [(.slice % 0 i)
-                        (.slice % (inc i)) ]))
-                  args)
-        args (into {} args) ]
-    {:path path :args args}) )
-
-#_(defn get-route []
-    (or  (@routes @(subscribe [:app])) (:default @routes) #{:disabled true}))
-
-(keys @routes)
-
-(defn main-app [content type]
-  (let [content (if (satisfies? IAtom content) @content content)]
-    (case type
-      (:app) [ui/app content]
-      (do
-        (log 'unsupported-app-type (:type content))
-        [:h1 "unsupported app type"]))))
-
-#_(defn start []
-    (let [adr (or
-                (and (= "#solsort:"  (.slice js/location.hash 0 9))
-                     (.slice js/location.hash 9))
-                (and (or (= js/location.port 1234)
-                         (= js/location.hostname "solsort.com")
-                         (= js/location.hostname "blog.solsort.com")
-                         (= js/location.hostname "localhost"))
-                     js/location.pathname))
-          route (and adr (parse-route adr))
-          ]
-      (dispatch-sync [:update-viewport])
-      (dispatch-sync [:route route])
-      (go
-        (let  [elem  (js/document.getElementById "solsort-app")
-               content (get-route)
-               content (if (function? content) (content) content)
-               content (if (chan? content) (<! content) content)
-               type (:type (if (satisfies? IAtom content) @content content))
-               ]
-          (when (and elem (not (:disabled content)))
-            (reagent/render-component  [main-app content type] elem))))))
-
 ;; # Actual routes for depended on code
 ;;
 ;; Modules used by the router cannot declare routes themself, so instead those routes 
 ;; are implemented here.
 
-(route 
+#_(route 
   "style" :http
   (fn []
     {:type :http
