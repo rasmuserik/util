369cb0db34d4f6318f6447bce6477bf32dd55fc8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-09-23 19:10:14 +0200

    progress on server-deploy-script

--- src/solsort/apps/install_sites.cljs
+++ src/solsort/apps/install_sites.cljs
@@ -19,21 +19,24 @@
 
 (when (and (some? js/window.require)
            (some? (js/window.require "fs")))
-  (defn <e [& args] 
-    (go (let [s (apply str args)  
+  (defonce has-error (atom false))
+
+  (defn <e [& args]
+    (go (let [s (apply str args)
               result (<! (<exec s))]
-          (log '> s) 
+          (log '> s)
           (when (nil? result)
+            (reset! has-error true)
             (log 'WARNING-FAILED s))
           result)))
   (defonce cfg (atom nil))
-  (defonce  fs (js/require "fs"))
-  (defonce  dl-path "/tmp/dl/")
-  (defonce  base-path "/tmp/new-solsort/")
+  (defonce fs (js/require "fs"))
+  (defonce dl-path "/tmp/dl/")
+  (defonce base-path "/tmp/new-solsort/")
 
   (defn <load-source [[k url]]
     (go
-      (let [fname (re-find #"[^/]*$" url) 
+      (let [fname (re-find #"[^/]*$" url)
             ext (re-find #"[^./]*$" url)]
         (<! (<e (str "install -d " dl-path ";"
                      "cd " dl-path ";"
@@ -60,29 +63,59 @@
         (<copy src dst))))
 
   (defn <exec-install [o path]
-    (go 
+    (go
       (<! (<e "install -d " path))
-      (<! (<seq<! 
+      (<! (<seq<!
             (map (fn [[src dest]]
                    (<install-content src (str path dest)))
                  (seq (get o :content [])))))
       (<! (<seq<!
-            (map 
+            (map
               #(<e "install -d " path % ";"
-                   "chmod 777 " path % ";") 
+                   "chmod 777 " path % ";")
               (get o :write-dir []))))
 
       (<! (<seq<!
-            (map 
+            (map
               (fn [[src dst]]
                 (<e "rm -rf " path dst ";"
                     "ln -sf " src " " path dst))
               (get o :ln []))))
       (<! (<seq<!
-            (map 
+            (map
               (fn [dir]
-                (<e "rsync -a /solsort/" dir "/ " path dir))
+                (<e 
+                  "install -d /solsort/" dir ";"
+                  "rsync -a /solsort/" dir "/ " path dir))
               (get o :preserve []))))))
+
+  (defn <wp-config [site]
+    (go ;TODO
+        false))
+
+  (defn <nginx-config [site]
+    (go ;TODO
+        false))
+
+  (defn <mysql-dbs [site]
+    (go (let [site ((:sites @cfg) site)
+              db (:db site)
+              user (:db-user site)
+              password (:db-password site)
+              db-pw (:db-root-password @cfg)]
+          (log 'mysql-init db user password)
+          (when (and db user password db-pw)
+            (<e "echo \""
+                "CREATE USER " user "@localhost IDENTIFIED BY '" password "';"
+                "FLUSH PRIVILEGES;" 
+                "\"| mysql mysql -u root --password=" db-pw ";"
+                "echo \""
+                "CREATE DATABASE " db ";"
+                "GRANT ALL PRIVILEGES ON " db ".* to " user "@localhost;"
+                "FLUSH PRIVILEGES;" 
+                "\"| mysql mysql -u root --password=" db-pw ";"
+                "true"
+                )))))
 
   (defn <install-site [site]
     (go (let [site-path (str base-path "sites/" site "/")]
@@ -90,33 +123,54 @@
           (log 'default-content-for site)
           (<! (<exec-install (:default-site @cfg) site-path))
           (log 'custom-content-for site)
-          (<! (<exec-install ((:sites @cfg) site) site-path)))))
+          (<! (<exec-install ((:sites @cfg) site) site-path))
+          (<! (<wp-config site))
+          (<! (nginx-config site))
+          (<! (<mysql-dbs site)))))
 
-  (route 
-    "install-sites"
-    (fn [o]
-      (when (nil? @cfg)
-        (reset! cfg true)
-        (go 
-          (log 'start-install)  
-          (<! (<e "cd /home/rasmuserik/install; git pull"))
-          ;(<! (<e "rm -rf " dl-path))
-          (<! (<e "rm -rf " base-path))
-          (let [config (-> fs
-                           (.readFileSync
-                             "/home/rasmuserik/install/config.clj")
-                           (js/String)
-                           (read-string))
-                config (assoc 
-                         config :sources
-                         (into {}
-                               (<! (<seq<! (map <load-source (:sources config))))))]
-            (reset! cfg config)
-            (<! (<exec-install (:root config) "/tmp/new-solsort/"))
-            (<! (<seq<! (map <install-site (keys (:sites @cfg)))))
-            )
-          (reset! cfg nil)
-          )
-        )
-      {:type :html
-       :html (log-elem)})))
+(defn <download-resources []
+  (go
+    ;(<! (<e "rm -rf " dl-path))
+    (let [config (-> fs
+                     (.readFileSync
+                       "/home/rasmuserik/install/config.clj")
+                     (js/String)
+                     (read-string))
+          config (assoc
+                   config :sources
+                   (into {}
+                         (<! (<seq<! (map <load-source (:sources config))))))]
+      (reset! cfg config))))
+(defn <create-sites []
+  (go
+    (log 'start-install)
+    (<! (<e "rm -rf " base-path))
+    (<! (<exec-install (:root @cfg) "/tmp/new-solsort/"))
+    (<! (<seq<! (map <install-site (keys (:sites @cfg)))))))
+
+(defn <install-sites  []
+  (go
+    (<! (<e "cd /home/rasmuserik/install; git pull"))
+    (<! (<download-resources))
+    (reset! has-error false)
+    (<! (<create-sites))
+    ; TODO: wp-config
+    (when (not @has-error)
+
+      (<! (<e "(crontab -l ; echo @reboot /solsort/html/start-server.sh)"
+              " | sort | uniq | crontab -"))
+      (<! (<e "sudo rm -rf /solsort-old" ))
+      (<! (<e "sudo mv /solsort /solsort-old" ))
+      (<! (<e "sudo mv /tmp/new-solsort /solsort")))
+    (reset! cfg nil)))
+
+(route 
+  "install-sites" 
+  (fn [o] 
+    (when (nil? @cfg)
+      (reset! cfg true)
+      (go
+        (<! (<install-sites))
+        (reset! cfg nil)))
+    {:type :html :html (log-elem)}))
+)
