b4c95b80f89b3d9371cc48810ad3819e5584004e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-10-03 10:41:58 +0200

    get urls from api

--- src/solsort/apps/hack4dk.cljs
+++ src/solsort/apps/hack4dk.cljs
@@ -9,6 +9,7 @@
     [re-frame.core :as re-frame :refer [subscribe register-sub register-handler dispatch dispatch-sync]]
 
     [solsort.util :refer [route log unique-id <p]]
+    [solsort.misc :refer [<seq<!]]
     [clojure.string :refer [replace split]]
     [solsort.net :refer [<ajax]]
     [solsort.ui :refer [app input default-shadow add-style icon]]
@@ -39,21 +40,31 @@
 ;        (clj->js (<! (<ajax  "http://testapi.natmus.dk/v1/Search/?query=(sourceId:106677)" ))))
 ;      (js/console.log 
 ;        (clj->js (<! (<ajax  "http://testapi.natmus.dk/v1/Search/?query=solvogn* and (categories:Rotationsbilleder)" )))))
+#_(go
+      (js/console.log 
+        (clj->js (<! (<ajax  "//solsort.com/natmusapi-proxy/v1/Search/?query=(categories:Rotationsbilleder)" )))))
 
-(defn <natmus [& args]
+
+(defn <natmus-id [id]
   (<ajax (str "//solsort.com/natmusapi-proxy"
-              "/v1/search/?query=" (apply str args))))
+              "/v1/search/?query=(sourceId:" id ")")))
 
 (defn <natmus-images [id]
   (log 'natmus-images id)
   (go 
-    (let [imgs (->> 
-                 (get (<! (<natmus "(sourceId:" id ")")) "Results")
+    (let [imgs (->> (get (<! (<natmus-id id)) "Results")
                 (map #(get % "relatedSubAssets"))
                 (filter #(< 0 (count %)))
-                (first))]
-    (js/console.log (clj->js imgs)))
-    []))
+                (first)
+                (map #(<natmus-id (get % "sourceId"))))
+          imgs (map #(get % "Results") (<! (<seq<! imgs)))
+          imgs (map 
+                 (fn [o] (->> o
+                    (map #(get % "assetUrlSizeMedium"))
+                    (filter #(< 0 (count %)))
+                    (first)))
+                 imgs)]
+      (or imgs []))))
 
 (route
   "360"
@@ -65,7 +76,7 @@
                       [])]
         {:type :html
          :html [:div 
-                (comment map (fn [im] [:img {:src im
+                (comment map (fn [im] [:img {:src (first (img-src))
                                              :width "100%"
                                              }]) (im-config "imgs"))
                 ]}))))
