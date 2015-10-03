7adead77f1e7f4c8df007c472bbe54c93081eed5
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-10-03 14:10:16 +0200

    first somewhat working version of 360

--- src/solsort/apps/hack4dk.cljs
+++ src/solsort/apps/hack4dk.cljs
@@ -23,14 +23,6 @@
                            "http://testapi.natmus.dk")]
           {:type :json :json (<! (<ajax url :result :json))}))))
 
-;(register-handler 
-;  :360-images
-;  (fn  [db  [_ imgs]]  
-;    (assoc-in db [:360 imgs] 
-;              (map 
-;                (fn [src] {:src src
-;                           :state :init })
-;                imgs))))
 ;
 ;(comment js/console.log 
 ;        (clj->js (<! (<ajax  "http://testapi.natmus.dk/v1/Search/?query=(categories:Rotationsbilleder)" )))
@@ -40,43 +32,83 @@
 ;        (clj->js (<! (<ajax  "http://testapi.natmus.dk/v1/Search/?query=(sourceId:106677)" ))))
 ;      (js/console.log 
 ;        (clj->js (<! (<ajax  "http://testapi.natmus.dk/v1/Search/?query=solvogn* and (categories:Rotationsbilleder)" )))))
-#_(go
-      (js/console.log 
-        (clj->js (<! (<ajax  "//solsort.com/natmusapi-proxy/v1/Search/?query=(categories:Rotationsbilleder)" )))))
-
 
 (defn <natmus-id [id]
-  (<ajax (str "//solsort.com/natmusapi-proxy"
+  (<ajax (str "//blog.solsort.com/natmusapi-proxy"
               "/v1/search/?query=(sourceId:" id ")")))
 
 (defn <natmus-images [id]
   (log 'natmus-images id)
-  (go 
-    (let [imgs (->> (get (<! (<natmus-id id)) "Results")
-                (map #(get % "relatedSubAssets"))
-                (filter #(< 0 (count %)))
-                (first)
-                (map #(<natmus-id (get % "sourceId"))))
-          imgs (map #(get % "Results") (<! (<seq<! imgs)))
-          imgs (map 
-                 (fn [o] (->> o
-                    (map #(get % "assetUrlSizeMedium"))
-                    (filter #(< 0 (count %)))
-                    (first)))
-                 imgs)]
-      (or imgs []))))
+  (go (let [imgs (->> (get (<! (<natmus-id id)) "Results")
+                      (map #(get % "relatedSubAssets"))
+                      (filter #(< 0 (count %)))
+                      (first)
+                      (map #(<natmus-id (get % "sourceId"))))
+            imgs (map #(get % "Results") (<! (<seq<! imgs)))
+            imgs (map (fn [o] (->> o
+                                   (map #(get % "assetUrlSizeMedium"))
+                                   (filter #(< 0 (count %)))
+                                   (first)))
+                      imgs)]
+        (or imgs []))))
 
-(route
-  "360"
-  (fn [o]
-    (go
-      (log o)
-      (let [[_ src id] (re-find #"^([^:]*):(.*)$" (o "src")) 
-            img-src (case src "natmus" (<! (<natmus-images id))
-                      [])]
-        {:type :html
-         :html [:div 
-                (comment map (fn [im] [:img {:src (first (img-src))
-                                             :width "100%"
-                                             }]) (im-config "imgs"))
-                ]}))))
+(defn preload-img [oid img-url]
+  (->  (js/document.createElement "img")   
+      (aset "onload" #(dispatch [:360-img-load oid img-url]))
+      (aset "src" img-url)))
+
+(register-handler 
+  :360-img-load
+  (fn  [db  [_ oid url]]  
+    (log 'im-load oid url)
+    (update-in db [:360 oid "loaded"] #(conj (or % #{}) url))))
+
+(register-handler 
+  :360-images 
+  (fn  [db  [_ oid imgs]]  
+    (doall (map preload-img oid imgs))
+    (assoc-in db [:360 oid :imgs] imgs)))
+
+(register-handler 
+  :360-pos
+  (fn  [db  [_ pid pos]]  (assoc-in db [:360-widget pid :pos] pos)))
+
+(register-sub :360-images (fn [db [_ oid]] (reaction (get-in @db [:360 oid] {}))))
+(register-sub :360-widget (fn [db [_ pid]] (reaction (get-in @db [:360-widget pid] {}))))
+
+(defn handle-move [pid e]
+  (let [client-x (aget e "clientX")
+        target (aget e "target")
+        target-width  (aget target "offsetWidth")]
+    (dispatch [:360-pos pid (* 2 (/ client-x target-width))])
+    (log pid client-x target-width)))
+
+(defn view-360 [pid oid]
+  (let [o @(subscribe [:360-images oid])
+        widget @(subscribe [:360-widget pid])]
+    (if-not (:imgs o)
+      [:div "Finding images"]
+      (let [imgs (:imgs o)
+            img-count (count imgs)
+            pos (-> (get widget :pos 0)
+                    (mod 1)
+                    (* (count imgs))
+                    (js/Math.floor))
+            ]
+
+        [:img {:src (nth (:imgs o) pos)
+               :on-mouse-move (partial handle-move pid)
+               :width "100%"}]))))
+
+(route "360"
+       (fn [o]
+         (go
+           (let [obj-id (o "src")
+                 [_ src id] (re-find #"^([^:]*):(.*)$" obj-id)
+                 pid (:id o)]
+             (go
+               (dispatch 
+                 [:360-images obj-id 
+                  (case src "natmus" (<! (<natmus-images id)) [])]))
+             {:type :html
+              :html [view-360 pid obj-id]}))))
