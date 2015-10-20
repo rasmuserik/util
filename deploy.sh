1e4af887f2b412c832649aa44edd67e57f8e768d
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-10-20 11:54:57 +0200

    solsort.js

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -vx
+killall java
 lein clean 
 npm install 
 lein cljsbuild once dist 
 cp resources/public/solsort.js ~/html/solsort.js 
 cp package.json ~/html/package.json
 cd ~/html/ 
-git commit -am solsort.js 
-git push 
-cd ~/solsort
+npm install && npm start
+echo "now if everything is ok:"
+echo "git commit -am solsort.js ; git push ; cd ~/solsort"
