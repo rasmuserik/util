469da964f73c5cae9b70a5d6e35be1e8f75bce6f
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-11-05 16:39:26 +0100

    notes+cleanup

--- misc/start-dev.sh
+++
@@ -1,7 +0,0 @@
-npm install
-./misc/gendoc.sh &
-#node ./misc/mubackend.js &
-npm start &
-lein clean 
-lein marg &
-lein figwheel
--- src/solsort/main.cljs
+++ src/solsort/main.cljs
@@ -1,28 +1,3 @@
-;; # solsort
-;;
-;; More notes/documentation to come here real soon.
-;;
-;; ## Backlog
-;;
-;; ## Build commands
-;;
-;; - `lein npm install` installs dependencies
-;; - `lein figwheel` starts development server on
-;;   [http://localhost:3449](http://localhost:3449/) with nrepl on port 7888.
-;; - `lein clean` removes artifacts etc
-;; - `lein kibit` and `lein bikeshed -m 1000` runs various style tests
-;; - `lein cljsbuild once dist` builds minified version
-;; - `lein gendoc` regenerate project README.md from literate source code
-;; - `lein ancient` check if dependencies are up to date
-;; - TODO `lein cljsbuild test` builds and run unit-tests
-;;
-;; ## Coding convention
-;;
-;; - async functions prefixed with `<` as suggested in http://www.lispcast.com/core-async-code-style
-;; ## Random ideas
-;;
-;; - selemium tests
-;;
 ;; # Main code
 ;;
 ;; This just loads all components and apps, and dispatches the route based on the app
