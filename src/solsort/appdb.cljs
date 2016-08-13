98bad6a0347b65a25a350d963d9bca7b6f00db81
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-13 13:10:54 +0200

    (db! path) removes element at path

--- src/solsort/appdb.cljs
+++ src/solsort/appdb.cljs
@@ -15,9 +15,9 @@
   "memoised function, that returns a subscription to a given path into the application db"
   (memoize db-raw))
 
-(defn db! "Write a value into the application db" [path value]
-  (swap! db-atom assoc-in path value)
-  value)
+  (defn db! "Write a value into the application db"
+    ([path] (swap! db-atom update-in (butlast path) dissoc (last path)))
+    ([path value] (swap! db-atom assoc-in path value) value))
 (defn db-async! [path value] (next-tick #(db! path value)))
 (defn db
   ([] @db-atom)
