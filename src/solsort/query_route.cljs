5296d2fdd17b2f0f2d6b1f6caf26df96bad41d5c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-15 16:51:52 +0200

    code cleanup

--- src/solsort/query_route.cljs
+++ src/solsort/query_route.cljs
@@ -6,34 +6,33 @@
    [solsort.misc :refer [throttle]]
    [solsort.appdb :refer [db db! db-async!]]))
 
-(defonce prev-url (atom nil))
-(defn add-history []
+(defonce prev-url
+  (atom nil))
+(defn add-history "Add the current route the the history"
+  []
   (when-not (= @prev-url js/location.href)
     (reset! prev-url js/location.href)
     (js/history.pushState nil nil js/location.href)))
-(def throttled-history (throttle add-history 2000))
-(defn- url-change []
-  (js/console.log (js/decodeURIComponent
-                   (or (second (re-find #"\?q=([^#]*)" js/location.href)) "{}")))
-  (db! [:query-route]
-       (try
-         (cljs.reader/read-string
-           (js/decodeURIComponent
-            (or (second (re-find #"\?q=([^#]*)" js/location.href)) "{}}")))
-         (catch js/Object e {}))))
-(defn- db-change []
-  (let [new-url (str
+(def throttled-add-history "add-history, but executing at most once every other second"
+  (throttle add-history 2000))
+
+(run! ; update url when :route changes
+ (let [new-url (str
                 (re-find #"[^#?]*" js/location.href)  "?q="
                 (js/encodeURIComponent
-                 (prn-str (db [:query-route]) )))
-        ]
-    (js/console.log "new-url" new-url (clj->js (db [:query-route] {})))
-    (when-not (= js/location.href new-url)
-      (js/history.replaceState nil nil new-url)
-      (throttled-history))))
+                 (prn-str (db [:route]))))]
+   (when-not (= js/location.href new-url)
+     (js/history.replaceState nil nil new-url)
+     (throttled-add-history))))
 
+(defn- url-change "update (db :route) when url changes" []
+  (js/console.log (js/decodeURIComponent
+                   (or (second (re-find #"\?q=([^#]*)" js/location.href)) "{}")))
+  (db! [:route]
+       (try
+         (cljs.reader/read-string
+          (js/decodeURIComponent
+           (or (second (re-find #"\?q=([^#]*)" js/location.href)) "nil")))
+         (catch js/Object e nil))))
 (url-change)
 (aset js/window "onpopstate" url-change)
-
-(defonce query-route-runner
-  (run! (db-change)))
