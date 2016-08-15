6a27cc29f4b272b9e1111da8531ba7580a7e180c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-15 17:21:25 +0200

    query as json in url, plus support both # and ?

--- src/solsort/query_route.cljs
+++ src/solsort/query_route.cljs
@@ -3,9 +3,11 @@
    [reagent.ratom :as ratom :refer [run!]])
   (:require
    [cljs.reader]
+   [clojure.walk]
    [solsort.misc :refer [throttle]]
    [solsort.appdb :refer [db db! db-async!]]))
 
+(defonce use-query (atom false))
 (defonce prev-url
   (atom nil))
 (defn add-history "Add the current route the the history"
@@ -16,23 +18,27 @@
 (def throttled-add-history "add-history, but executing at most once every other second"
   (throttle add-history 2000))
 
+(defn hex-byte [i] (.slice (.toString (bit-or i 256) 16) -2))
 (run! ; update url when :route changes
  (let [new-url (str
-                (re-find #"[^#?]*" js/location.href)  "?q="
-                (js/encodeURIComponent
-                 (prn-str (db [:route]))))]
-   (when-not (= js/location.href new-url)
+                (re-find #"[^#?]*" js/location.href)  (if @use-query "?" "#")
+                (clojure.string/replace
+                 (JSON.stringify (clj->js (db [:route])))
+                 (js/RegExp. "[%#&?]" "g")
+                 (fn [c] (str "%" (hex-byte (.charCodeAt c 0))))))]
+   (when (and (db [:route])
+              (not (= js/location.href new-url)))
      (js/history.replaceState nil nil new-url)
      (throttled-add-history))))
 
 (defn- url-change "update (db :route) when url changes" []
-  (js/console.log (js/decodeURIComponent
-                   (or (second (re-find #"\?q=([^#]*)" js/location.href)) "{}")))
   (db! [:route]
        (try
-         (cljs.reader/read-string
-          (js/decodeURIComponent
-           (or (second (re-find #"\?q=([^#]*)" js/location.href)) "nil")))
+         (clojure.walk/keywordize-keys
+          (js->clj
+           (JSON.parse
+            (js/decodeURIComponent
+             (or (second (re-find #"[?#](.*)" js/location.href)) "")))))
          (catch js/Object e nil))))
 (url-change)
 (aset js/window "onpopstate" url-change)
