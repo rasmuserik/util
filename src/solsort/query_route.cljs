181706353f6d3186f09808dbb6a0dd0ed6bc8a61
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-16 17:15:39 +0200

    better naming

--- src/solsort/query_route.cljs
+++ src/solsort/query_route.cljs
@@ -21,9 +21,9 @@
    (add-history)))
 (def throttled-add-history "add-history, but executing at most once every other second"
   (throttle add-history 2000))
-(defn set-route [o] (db! @path o))
-(defn route [] (db @path))
-(defn route-url [o]
+(defn open [o] (db! @path o))
+(defn data [] (db @path))
+(defn url [o]
   (str
    (re-find #"[^#?]*" js/location.href)  (if @use-query "?" "#")
    (clojure.string/replace
@@ -33,7 +33,7 @@
 (defn set-route-path [p] (reset! path p))
 
 (run! ; update url when :route changes
- (let [new-url (route-url (db @path))]
+ (let [new-url (url (db @path))]
    (when (and (db @path)
               (not (= js/location.href new-url)))
      (js/history.replaceState nil nil new-url)
